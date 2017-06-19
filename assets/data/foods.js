7a88dddf40cf6040cf9fa604e546230f9c48c10b
Radek <radoslaw_smyk@wp.pl>
2017-06-19 21:18:43 +0200

    falling elements wip

---
+++ assets/data/foods.js
@@ -0,0 +1,13 @@
+foods = [
+  {id: 1, name: 'banana', category: 'good', photo: 'img/banana.png'},
+  {id: 2, name: 'apple', category: 'good', photo: 'img/apple.png'},
+  {id: 3, name: 'avocado', category: 'good', photo: 'img/avocado.png'},
+  {id: 4, name: 'brocly', category: 'good', photo: 'img/brocly.png'},
+  {id: 5, name: 'carrot', category: 'good', photo: 'img/carrot.png'},
+  {id: 6, name: 'fish', category: 'good', photo: 'img/fish.png'},
+  {id: 7, name: 'burger', category: 'bad', photo: 'img/burger.png'},
+  {id: 8, name: 'candy', category: 'bad', photo: 'img/candy.png'},
+  {id: 9, name: 'donut', category: 'bad', photo: 'img/donut.png'},
+  {id: 10, name: 'pizza', category: 'bad', photo: 'img/pizza.png'},
+
+  ]
--- main.js
+++ main.js
@@ -1,4 +1,61 @@
-$(document).ready(function () {
+
+  //creating elements
+  var interval = 3000;
+    function Food(speed, init) {
+  var food = this;
+
+    food.animateSpeed = speed;
+    food.startX = Math.floor(Math.random() * 765) + 500;
+    food.startY = 0;
+    food.itemNumber = Math.round(Math.random()*10)
+    food.foodId = foods.filter(function(item) {
+      if (item.id == food.itemNumber) {
+        return item.id;
+      }
+    });
+    food.footphoto = foods.filter(function(item){
+      if (item.id == food.itemNumber) {
+        return item.photo;
+      }
+    })
+
+    this.createElement = function () {
+      this.htmlElement = document.createElement('div');
+      this.htmlElement.style.top = food.startY + "px";
+      this.htmlElement.style.left = food.startX + "px";
+      this.htmlElement.className = 'foodItem ' + food.foodId[0].category + ' ' + food.foodId[0].name;
+      this.htmlElement.id = food.foodId[0].id;
+      document.body.appendChild(this.htmlElement);
+    }
+
+    this.animate = function (speed) { // show function in FrogsAndSharksGame
+      if (!this.htmlElement) {
+        console.log('Create HTML element first!');
+        return;
+      }
+      $(this.htmlElement).animate({
+        top: "100vh"
+      }, speed, 'linear', () => {
+        this.remove();
+      });
+    }
+
+    this.remove = function () {
+      this.htmlElement.remove();
+    }
+
+    this.init = function () {
+      this.createElement();
+      this.animate(3500);
+    }
+
+    if (init !== false) {
+      this.init();
+    }
+
+  }
+
+  game = setInterval(Food, interval)
 
 // player can run and jump
   var player = {};
@@ -32,12 +89,12 @@ $(document).ready(function () {
         ;
         break;
       case 38:
-        $('#character').animate({"top": (player.y -= 30) + 'px' }, 200);
-        $('#character').animate({"top": (player.y += 30) + 'px' }, 200);
+        $('#character').animate({"top": (player.y -= 30) + 'px'}, 200);
+        $('#character').animate({"top": (player.y += 30) + 'px'}, 200);
         break;
     }
   }
-})
+
 
 
 
