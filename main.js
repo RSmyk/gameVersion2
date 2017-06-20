5a61d7387fbf2dd4ee9157011e759d761407f32e
Radek <radoslaw_smyk@wp.pl>
2017-06-20 14:54:12 +0200

    falling items with dimensions

--- main.js
+++ main.js
@@ -1,100 +1,115 @@
+$(document).ready(function () {
 
-  //creating elements
-  var interval = 3000;
-    function Food(speed, init) {
-  var food = this;
+  $container = document.getElementById('container')
 
-    food.animateSpeed = speed;
-    food.startX = Math.floor(Math.random() * 765) + 500;
-    food.startY = 0;
-    food.itemNumber = Math.round(Math.random()*10)
-    food.foodId = foods.filter(function(item) {
-      if (item.id == food.itemNumber) {
-        return item.id;
-      }
-    });
-    food.footphoto = foods.filter(function(item){
-      if (item.id == food.itemNumber) {
-        return item.photo;
-      }
-    })
 
-    this.createElement = function () {
-      this.htmlElement = document.createElement('div');
-      this.htmlElement.style.top = food.startY + "px";
-      this.htmlElement.style.left = food.startX + "px";
-      this.htmlElement.className = 'foodItem ' + food.foodId[0].category + ' ' + food.foodId[0].name;
-      this.htmlElement.id = food.foodId[0].id;
-      document.body.appendChild(this.htmlElement);
-    }
+startGame();
 
-    this.animate = function (speed) { // show function in FrogsAndSharksGame
-      if (!this.htmlElement) {
-        console.log('Create HTML element first!');
-        return;
-      }
-      $(this.htmlElement).animate({
-        top: "100vh"
-      }, speed, 'linear', () => {
-        this.remove();
-      });
-    }
+  // declaration variables
+  var newFood, x, y, idInterval
 
-    this.remove = function () {
-      this.htmlElement.remove();
-    }
+// random falling elements
 
-    this.init = function () {
-      this.createElement();
-      this.animate(3500);
-    }
+ function startGame () {
+   idInterval = setInterval(
+     function game() {
+       //random position of newFood
+       x = Math.floor(Math.random() * 40);
+       y = 0;
 
-    if (init !== false) {
-      this.init();
-    }
+//generating a div
 
-  }
+newFood = document.createElement('div')
+       // setting new div position
+       newFood.style.top = y + "vh";
+       newFood.style.left = x + "vw";
 
-  game = setInterval(Food, interval)
+       // generating random number from 1 to 10
+       var randomFoods = (Math.round(Math.random() * 9) + 1);
+
+       //get id of foods
+       var foodId = [];
+       foodId = foods.filter(function (item) {
+         if (item.id == randomFoods) {
+           return foodId.push(item)
+
+         }
+       });
+
+// we give random class to our empty object
+       newFood.className = 'foodItem';
+
+
+// set id for all divs
+       newFood.id = foodId[0].name;
+
+       // attaching new personalized div to body
+       $container.append(newFood);
+
+       // animate falling | speed = time in ms needed to fall from top to bottom | when it reaches bottom, function will delete the object
+       animate(newFood, 100);
+       function animate(element, speed) {
+         var y = 0;
+         var id = setInterval(function() {
+          y++
+           element.style.top = y + 'vh';
+// console.log(element.style.top)
+           if (parseInt(element.style.top )> 67 ) {
+             element.remove();
+             clearInterval(id);
+           }
+
+
+
+         }, speed)
+       }
+
+     },1500
+   )
+ }
+
+
+
 
 // player can run and jump
-  var player = {};
-  player.x = $('#container').width() / 2;
-  player.y = 600;
+  var player = $('#character');
+  player.x = 20
+  player.y = 51
   $(document).on('keydown', handleTyping);
   function handleTyping(e) {
     switch (e.which) {
       case 39:
-        if (player.x > 730) {
+        if (player.x > 35) {
           $('#character').css({
-            'left': (player.x += 0) + 'px'
+            'left': (player.x += 0) + 'vw'
           });
         } else {
           $('#character').css({
-            'left': (player.x += 15) + 'px'
+            'left': (player.x += 1) + 'vw'
           })
         }
         ;
         break;
       case 37:
-        if (player.x < 7) {
+        if (player.x < 1) {
           $('#character').css({
-            'left': (player.x -= 0) + 'px'
+            'left': (player.x -= 0) + 'vw'
           });
         } else {
           $('#character').css({
-            'left': (player.x -= 15) + 'px'
+            'left': (player.x -= 1) + 'vw'
           })
         }
         ;
         break;
       case 38:
-        $('#character').animate({"top": (player.y -= 30) + 'px'}, 200);
-        $('#character').animate({"top": (player.y += 30) + 'px'}, 200);
+        $('#character').animate({"top": (player.y -= 3) + 'vh'}, 200);
+        $('#character').animate({"top": (player.y += 3) + 'vh'}, 200);
         break;
     }
   }
 
 
+})
 
 
